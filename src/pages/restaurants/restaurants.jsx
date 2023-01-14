a55e2e42d085ade2bb9a040f50c4287ea80c2b1b
jeezzas <mostafahakkou1999@gmail.com>
2023-01-14 22:55:56 +0100

    restaurants-start

--- src/pages/restaurants/restaurants.jsx
+++ src/pages/restaurants/restaurants.jsx
@@ -1,7 +1,85 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+import './restaurants.scss'
+
 function Restaurants(){
+
+    const [restaurants, setRestaurants] = useState();
+
+    async function getRestaurants(){
+        const restaurants =  await axios.get('http://165.22.116.57:3000/restaurant/restaurantVitrine');
+        setRestaurants(restaurants.data);
+
+
+    }
+    useEffect(()=>{
+        getRestaurants();
+    },[])
+    console.log(restaurants)
     return(
-        <>
-        </>  
+        <div className="container">
+            <div className='title_container'>
+            <h3>Decouvre nous restuarants </h3>
+            </div>
+            <div className="cards_container">
+                {restaurants && 
+                   <div className="restaucard" >
+                    <img src={`http://165.22.116.57:3000/${restaurants[0].logo}`} className='restaulogo'/>
+                    <div className='bannertext'> 
+                        <img src={`http://165.22.116.57:3000/${restaurants[0].banner}`}/>
+                    <div className="gradient"></div>
+                  <h4>{restaurants[0].nom}</h4>
+                  
+                    </div>
+                </div>
+                }
+                      {restaurants && 
+                   <div className="restaucard" >
+                    <img src={`http://165.22.116.57:3000/${restaurants[0].logo}`} className='restaulogo'/>
+                    <div className='bannertext'> 
+                        <img src={`http://165.22.116.57:3000/${restaurants[0].banner}`}/>
+                    <div className="gradient"></div>
+                  <h4>{restaurants[0].nom}</h4>
+                  
+                    </div>
+                </div>
+                }
+                      {restaurants && 
+                   <div className="restaucard" >
+                    <img src={`http://165.22.116.57:3000/${restaurants[0].logo}`} className='restaulogo'/>
+                    <div className='bannertext'> 
+                        <img src={`http://165.22.116.57:3000/${restaurants[0].banner}`}/>
+                    <div className="gradient"></div>
+                  <h4>{restaurants[0].nom}</h4>
+                  
+                    </div>
+                </div>
+                }
+                      {restaurants && 
+                   <div className="restaucard" >
+                    <img src={`http://165.22.116.57:3000/${restaurants[0].logo}`} className='restaulogo'/>
+                    <div className='bannertext'> 
+                        <img src={`http://165.22.116.57:3000/${restaurants[0].banner}`}/>
+                    <div className="gradient"></div>
+                  <h4>{restaurants[0].nom}</h4>
+                  
+                    </div>
+                </div>
+                }
+                      {restaurants && 
+                   <div className="restaucard" >
+                    <img src={`http://165.22.116.57:3000/${restaurants[0].logo}`} className='restaulogo'/>
+                    <div className='bannertext'> 
+                        <img src={`http://165.22.116.57:3000/${restaurants[0].banner}`}/>
+                    <div className="gradient"></div>
+                  <h4>{restaurants[0].nom}</h4>
+                  
+                    </div>
+                </div>
+                }
+               
+            </div>
+        </div>  
     )
 }
 
